b5d41a13c916e6d81b74807090c8be02b2443fa8
Tristan Myers <tristanbmyers@gmail.com>
2018-03-03 15:12:30 -0500

    Updated Model

---
+++ StepUpNYC/Models/Answer.swift
@@ -0,0 +1,14 @@
+//
+//  Answer.swift
+//  StepUpNYC
+//
+//  Created by MacBook on 3/3/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import Foundation
+
+struct Answer: Codable {
+    let isCorrect: Bool?
+    let text: String?
+}
