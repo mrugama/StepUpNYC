150184a483ef7aed90ee39e296f2660b0791cc41
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2018-03-04 11:45:21 -0500

    working on the questionary

--- StepUpNYC/Models/Answer.swift
+++ StepUpNYC/Models/Answer.swift
@@ -7,9 +7,32 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 struct Answer: Codable {
     let questionId: String?
     let isCorrect: Bool?
     let text: String?
+    
+    // Data from Firebase
+    init(snapShot: DataSnapshot) {
+        let value = snapShot.value as? [String: Any]
+        self.questionId = value?["text"] as? String ?? ""
+        self.isCorrect = value?["isCorrect"] as? Bool ?? false
+        self.text = value?["text"] as? String ?? ""
+    }
+    
+    // convert into data model
+    init(questionId: String, isCorrect: Bool, text: String) {
+        self.questionId = questionId
+        self.isCorrect = isCorrect
+        self.text = text
+    }
+    
+    // convert into JSON format
+    func toAnyObj() -> [String: Any] {
+        return ["questionId": questionId ?? "",
+                "isCorrect": isCorrect ?? false,
+                "text": text ?? ""]
+    }
 }
