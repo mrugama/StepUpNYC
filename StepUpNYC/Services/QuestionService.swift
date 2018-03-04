bbe40de93515d5189a9028f1a0f5ec65c5150e0e
Tristan Myers <tristanbmyers@gmail.com>
2018-03-03 19:18:56 -0500

    Marlon told me to do this

--- StepUpNYC/Services/QuestionService.swift
+++ StepUpNYC/Services/QuestionService.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2018 C4Q. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import FirebaseDatabase
+
+struct QuestionService {
+    private init() {
+        dbRef = Database.database().reference()
+        questionRef = dbRef.child("questions")
+        answerRef = questionRef.child("answers")
+    }
+    static let manager = QuestionService()
+    private let dbRef: DatabaseReference
+    private let questionRef: DatabaseReference
+    private let answerRef: DatabaseReference
+    
+    func getQuestion(completion: @escaping ([Question]) -> Void) {
+        let ref = questionRef
+        var questions = [Question]()
+        ref.observe(.value) { (snapShots) in
+            for value in snapShots.children {
+                let newQ = Question(snapShot: value as! DataSnapshot)
+                questions.append(newQ)
+            }
+            completion(questions)
+        }
+    }
+    
+    func saveQuestion(text: String, explanation: String, image: UIImage?, answers: [Answer]) {
+        let ref = Database.database().reference().child("questions")
+        let questionRef = ref.childByAutoId()
+        let newQ = Question(text: text, explanation: explanation, imageURL: nil)
+        questionRef.setValue(newQ.toAnyObject())
+        let newA = ref.child(questionRef.ref.key).child("answers").childByAutoId()
+        for answer in answers {
+            newA.setValue(answer.toAnyObject())
+        }
+    }
+}
